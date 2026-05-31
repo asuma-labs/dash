be08500dd1a2518b2c42533f0e103fa3f1e8707b
asuma-labs <asuma.my.id@gmail.com>
2026-06-01 04:20:27 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,30 +1,14 @@
+// frontend/next.config.ts
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'cdn.asuma.my.id',
-                port: '',
-                pathname: '/**',
-            },
-            {
-                protocol: 'https',
-                hostname: '**.vercel.app',
-                port: '',
-                pathname: '/**',
-            },
-        ],
-    },
-    output: 'standalone',
-    swcMinify: true,
-    compress: true,
-    poweredByHeader: false,
-    reactStrictMode: true,
-    experimental: {
-        optimizePackageImports: ['axios', 'jwt-decode'],
-    },
+  images: {
+    remotePatterns: [{ protocol: 'https', hostname: 'cdn.asuma.my.id' }],
+  },
+  output: 'standalone',
+  compress: true,
+  poweredByHeader: false,
+  reactStrictMode: true,
 };
 
 export default nextConfig;
